ad7bef9632588261065f8e42dcb7920fc65c88ac
Pau hidalgo <ubuntu@ip-172-31-89-193.ec2.internal>
2023-06-11 21:16:30 +0000

    Añadir a src/requirements.txt urslib3<2, gracias a Francisco y así poder hacer funcionar la api en local. Comentar algunas funciones

--- src/update.py
+++ src/update.py
@@ -5,13 +5,23 @@ import todoList
 
 
 def update(event, context):
+    """Update an existing todo element.
+    Identified by path parameter. 
+    Path: PUT todos/{id}
+    Mandatory to receive JSON format and required at least "text" property on JSON payload.
+    
+    :event: environment variables such as payload, path paramethers, etc. 
+    :context: --
+    :raise Exception if mandatory fields are not received on JSON Payload.    
+    :return: An object with two properties: (int) statusCode and (string) body.
+    """
     data = json.loads(event['body'])
     if 'text' not in data or 'checked' not in data:
         logging.error("Validation Failed")
         raise Exception("Couldn't update the todo item.")
         return
     # update the todo in the database
-    result = todoList.update_item(
+    result = todoList.update_item( 
         event['pathParameters']['id'],
         data['text'], data['checked'])
     # create a response
